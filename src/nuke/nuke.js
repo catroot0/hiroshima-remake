de22d0ac2f1c765b75a188afb2ea80b50ad5d6ff
drowning14 <drowning.dev1@gmail.com>
2025-03-10 23:21:46 +0330

    OMG IT FINALLY FIXED,
    deleting roles and channels together for more speed

--- src/nuke/nuke.js
+++ src/nuke/nuke.js
@@ -6,8 +6,14 @@ import { selectedGuild } from "../alert/getGuild.js";
 export default async function nuke() {
   const guild = await client.guilds.fetch(selectedGuild.id);
   const bot = await guild.members.fetch(client.user.id);
+
   if (!(await checkPermissions(bot))) return;
-  await RoleManager.deleteAllRoles(guild);
-  await ChannelManager.deleteAllChannels(guild);
+
+  const deleteRoles = RoleManager.deleteAllRoles(guild);
+  const deleteChannels = ChannelManager.deleteAllChannels(guild);
+
+  await deleteRoles;
+  await deleteChannels;
+
   await ChannelManager.createChannel(guild);
 }
